90e3c3e24216b4b2a3aa99af5a3d38d49471c8bb
Thomas Arellano <Thomas.G.Arellano@gmail.com>
2016-07-02 14:34:24 -0400

    Add KMapView

---
+++ app/src/main/java/com/tarellano/kayak/views/KMapView.java
@@ -0,0 +1,13 @@
+package com.tarellano.kayak.views;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Thomas Arellano on 7/2/2016.
+ */
+public class KMapView extends View {
+    public KMapView(Context context) {
+        super(context);
+    }
+}
