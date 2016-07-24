fec8c67077bca8c1fdbc77caa5501314c0e3f37b
Thomas Arellano <Thomas.G.Arellano@gmail.com>
2016-07-24 18:08:20 -0400

    Add custom view touch event

--- app/src/main/java/com/tarellano/kayak/views/KMapView.java
+++ app/src/main/java/com/tarellano/kayak/views/KMapView.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.tarellano.kayak.R;
 
 /**
  * Created by Thomas Arellano on 7/2/2016.
@@ -12,21 +17,42 @@ import android.view.View;
 public class KMapView extends View {
 
     private Paint mBackgroundPaint;
+    private Paint mLinePaint;
+
+
+    private Path mPath = new Path();
 
     public KMapView(Context context) {
         super(context);
         init();
     }
 
+    public KMapView(Context context, AttributeSet st) {
+        super(context, st);
+        init();
+    }
+
     private void init(){
         mBackgroundPaint = new Paint(0);
-        mBackgroundPaint.setColor(0xff101010);
+        mBackgroundPaint.setColor(0xff515151);
         mBackgroundPaint.setMaskFilter(new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL));
+        mBackgroundPaint.setAntiAlias(true);
+        mBackgroundPaint.setStyle(Paint.Style.STROKE);
+
+        mLinePaint = new Paint(0);
+        mLinePaint.setColor(getResources().getColor(R.color.bluegrass));
+        mLinePaint.setStyle(Paint.Style.FILL);
 
     }
 
     @Override
     public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        //canvas.drawLine(0,0, getWidth(), getHeight(), mBackgroundPaint);
+
+        canvas.drawRect(0, 0, getWidth(), getHeight(), mLinePaint);
+        canvas.drawPath(mPath, mBackgroundPaint);
 
     }
 
@@ -37,5 +63,24 @@ public class KMapView extends View {
 
         float ww = (float)w - xpad;
         float hh = (float)h - ypad;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        float touchX = motionEvent.getX();
+        float touchY = motionEvent.getY();
+
+        switch(motionEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        invalidate();
+        return true;
     }
 }
